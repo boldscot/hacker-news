a85375a26d57b6e8677d271efb11878189333dbd
Stephen <bold-scot@hotmail.com>
2022-03-11 22:26:46 +0000

    Added max item function test

--- src/app/services/hacker-news-service/hacker-news.service.spec.ts
+++ src/app/services/hacker-news-service/hacker-news.service.spec.ts
@@ -1,7 +1,8 @@
+import { environment } from './../../../environments/environment';
 import { TestBed } from '@angular/core/testing';
 
 import { HackerNewsService } from './hacker-news.service';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 
 describe('HackerNewsService', () => {
   let service: HackerNewsService;
@@ -11,6 +12,8 @@ describe('HackerNewsService', () => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
+      ],
+      providers: [
         HttpTestingController
       ]
     });
@@ -21,4 +24,26 @@ describe('HackerNewsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#getMaxItemId() should return Observable<number>', () => {
+    const maxId = 9100012;
+
+    service.getMaxItemId().subscribe((response: number) => {
+      expect(response).toBeDefined();
+      expect(response).toBe(maxId);
+    });
+
+    let req: TestRequest = httpTestingController.expectOne(`${environment.hackerNewsUrl}/maxitem.json`);
+    expect(req.request.method).toBe('GET');
+    req.flush(maxId);
+    httpTestingController.verify();
+
+    service.getMaxItemId().subscribe((response: number) => {
+      expect(response).toBeNull();
+    });
+
+    req = httpTestingController.expectOne(`${environment.hackerNewsUrl}/maxitem.json`);
+    req.flush(null, {status: 401, statusText: 'Unauthorised'});
+    httpTestingController.verify();
+  })
 });
