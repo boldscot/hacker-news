9dd7bf4001b85687f7635833b6e568a2ad39edb4
Stephen <bold-scot@hotmail.com>
2022-03-11 22:02:08 +0000

    Added testing controller

--- src/app/services/hacker-news-service/hacker-news.service.spec.ts
+++ src/app/services/hacker-news-service/hacker-news.service.spec.ts
@@ -1,18 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HackerNewsService } from './hacker-news.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('HackerNewsService', () => {
   let service: HackerNewsService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        HttpTestingController
       ]
     });
     service = TestBed.inject(HackerNewsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
