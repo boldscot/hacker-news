871a65ad22b56d8d11b1e9c3ad65c9e6f82cf4f6
Stephen <bold-scot@hotmail.com>
2022-03-11 21:50:05 +0000

    Added testing module to service spec imports

--- src/app/services/hacker-news-service/hacker-news.service.spec.ts
+++ src/app/services/hacker-news-service/hacker-news.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HackerNewsService } from './hacker-news.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HackerNewsService', () => {
   let service: HackerNewsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
     service = TestBed.inject(HackerNewsService);
   });
 
