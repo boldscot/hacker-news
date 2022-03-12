9e213551f15c87bb929a3ee9ce4876ebef8b0924
Stephen <bold-scot@hotmail.com>
2022-03-12 00:25:16 +0000

    Added get top stories implementation

--- src/app/services/hacker-news-service/hacker-news.service.ts
+++ src/app/services/hacker-news-service/hacker-news.service.ts
@@ -26,15 +26,23 @@ export class HackerNewsService {
     );
   }
 
-
+  /**
+   * Makes a GET request to get the top stories, which is an array of ids, from the Hacker News API
+   * @returns Observable<number[]> or Observable<null>
+   */
   getTopStories(): Observable<number[] | null> {
-    return of([1,2,3,4])
+    return this.http.get<number[]>(`${environment.hackerNewsUrl}/topstories.json`, {
+      observe: 'response'
+    }).pipe(
+      map((response: HttpResponse<number[]>) => {
+        return (response && response.status === 200)? response.body: null;
+      }),
+      catchError((err: HttpErrorResponse)=> this.handleError(err))
+    );
   }
 
-
-
   /**
-   * Simple error handler fucntion, logs the error message and returns
+   * Simple error handler function, logs the error message and returns
    * @param err The HttpErrorResponse from the request
    * @returns Observable<null>
    */
