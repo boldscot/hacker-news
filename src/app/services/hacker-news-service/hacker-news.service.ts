5d6e7a712206010c8cfeeeb47a16fb211dd44886
Stephen <bold-scot@hotmail.com>
2022-03-13 01:11:10 +0000

    Removed import

--- src/app/services/hacker-news-service/hacker-news.service.ts
+++ src/app/services/hacker-news-service/hacker-news.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpErrorResponse, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { environment } from './../../../environments/environment';
