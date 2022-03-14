0d2452e0cc00600e93f898f168d6dc561f150daf
Stephen <bold-scot@hotmail.com>
2022-03-14 23:04:24 +0000

    Added item comp tests

--- src/app/components/item-component/item/item.component.spec.ts
+++ src/app/components/item-component/item/item.component.spec.ts
@@ -1,3 +1,5 @@
+import { MockHackerNewsService } from './../../../testutils/mock-hacker-news-service';
+import { HackerNewsService } from 'src/app/services/hacker-news-service/hacker-news.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
@@ -10,6 +12,9 @@ fdescribe('ItemComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HackerNewsService, useClass: MockHackerNewsService }
+      ],
       declarations: [ ItemComponent ]
     });
 
@@ -20,5 +25,26 @@ fdescribe('ItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should invoke the getItem() function when the itemId setter is populated', () => {
+    let wasInvoked: boolean = false;
+    spyOn(component, 'getItem').and.callFake(() => wasInvoked = true);
+    component.itemId = 1;
+    fixture.detectChanges();
+    expect(wasInvoked).toBeTruthy();
+  });
+
+  it('should populate the item$ observable when the itemId setter is populated', () => {
+    component.item$.subscribe(data => expect(data).toBeNull());
+    component.itemId = 1;
+    component.item$.subscribe(data => {
+      expect(data).not.toBeNull();
+      expect(data?.id).toBe(1);
+      expect(data?.type).toEqual('story');
+      expect(data?.title?.toLowerCase()).toContain('story item')
+    });
+    component.itemId = 3;
+    component.item$.subscribe(data => expect(data).toBeNull());
   });
 });
