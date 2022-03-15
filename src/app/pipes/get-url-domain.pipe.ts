6a30aee7eac2e9b9e61e78b8dec03505ae0743ac
Stephen <bold-scot@hotmail.com>
2022-03-15 22:04:05 +0000

    Added comment

--- src/app/pipes/get-url-domain.pipe.ts
+++ src/app/pipes/get-url-domain.pipe.ts
@@ -1,5 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+/**
+ * This pipe takes a url string and returns the domain part of the url.
+ * e.g  Input => www.domain.ie/v2/user?id=1000
+ *      Output => domain.ie
+ */
 @Pipe({name: 'getUrlDomain'})
 export class GetUrlDomainPipe implements PipeTransform {
   transform(url: string) {
