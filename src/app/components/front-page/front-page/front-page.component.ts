949f64d22d9c9dc05d8f22a3e482fc966166f548
Stephen <bold-scot@hotmail.com>
2022-03-16 00:06:29 +0000

    Added comments

--- src/app/components/front-page/front-page/front-page.component.ts
+++ src/app/components/front-page/front-page/front-page.component.ts
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./front-page.component.scss']
 })
 export class FrontPageComponent {
+  /**
+   * Property that is passed to the item grid component's 'storyType' input property
+   * @type {('topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories')}
+   * @memberof FrontPageComponent
+   */
   storyType: 'topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories';
 
   constructor() {
     this.storyType = 'topstories';
   }
 
+  /**
+   * Event handler for the storyType emitter in the toolbar component
+   * @param type The story type that was selected on the toolbar
+   */
   onStoryClickHandler(type: 'topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories') {
     if (this.storyType !== type) this.storyType = type;
   }
