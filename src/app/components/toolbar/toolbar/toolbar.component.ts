c8e3973a4645f4487a6cbf1cf0eb885a7d6d6fde
Stephen <bold-scot@hotmail.com>
2022-03-16 00:12:36 +0000

    Added comments

--- src/app/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar/toolbar.component.ts
@@ -6,7 +6,19 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  /**
+   * The Event Emitter passes the emiited value to its parent
+   * The Emitter emits a value when a story is selected from the toolbar
+   * @type {(EventEmitter<'topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories'>)}
+   * @memberof ToolbarComponent
+   */
   @Output() storyTypeSelectEmitter: EventEmitter<'topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories'>;
+
+  /**
+   * Array of literal types used in the temnpate to display them in the toolbar
+   * @type {(('topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories')[])}
+   * @memberof ToolbarComponent
+   */
   storyTypes: ('topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories')[]
     = ['topstories' , 'newstories' , 'beststories' , 'askstories' , 'showstories' , 'jobstories'];
 
@@ -14,6 +26,10 @@ export class ToolbarComponent {
     this.storyTypeSelectEmitter = new EventEmitter();
   }
 
+  /**
+   * Event handler for the click event on the toolbar story types
+   * @param storyType The story type selected from the toolbar
+   */
   onStoryClickHandler(storyType: 'topstories' | 'newstories' | 'beststories' | 'askstories' | 'showstories' | 'jobstories') {
     this.storyTypeSelectEmitter.emit(storyType);
   }
