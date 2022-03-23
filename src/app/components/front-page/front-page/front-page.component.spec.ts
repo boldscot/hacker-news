c28997e00e63d5a41555b6c63b7075778c9deb1f
Stephen <bold-scot@hotmail.com>
2022-03-23 20:37:16 +0000

    Reafctored front page tests

--- src/app/components/front-page/front-page/front-page.component.spec.ts
+++ src/app/components/front-page/front-page/front-page.component.spec.ts
@@ -1,38 +1,17 @@
-import { PipesModule } from './../../../pipes/pipes.module';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { ItemComponent } from './../../item-component/item/item.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MockHackerNewsService } from './../../../testutils/mock-hacker-news-service';
-import { HackerNewsService } from './../../../services/hacker-news-service/hacker-news.service';
-import { ItemGridComponent } from './../../item-grid/item-grid.component';
-import { ToolbarComponent } from './../../toolbar/toolbar/toolbar.component';
 import { StoryType } from './../../../customtypes/story-type';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FrontPageComponent } from './front-page.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('FrontPageComponent', () => {
+fdescribe('FrontPageComponent', () => {
   let component: FrontPageComponent;
   let fixture: ComponentFixture<FrontPageComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        FrontPageComponent,
-        ToolbarComponent,
-        ItemGridComponent,
-        ItemComponent
-      ],
-      providers: [
-        {provide: HackerNewsService, useClass: MockHackerNewsService}
-      ],
-      imports: [
-        MatToolbarModule,
-        MatIconModule,
-        MatGridListModule,
-        PipesModule
-      ]
+      declarations: [ FrontPageComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
     });
 
     fixture = TestBed.createComponent(FrontPageComponent);
