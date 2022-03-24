ea27f995fb56e64bd77415d0e087ad9ab430a94d
Stephen <bold-scot@hotmail.com>
2022-03-24 00:14:12 +0000

    Added menu and styling

--- src/app/components/toolbar/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar/toolbar.component.ts
@@ -1,12 +1,24 @@
+import { BreakpointState, Breakpoints } from '@angular/cdk/layout';
+import { GridLayoutService } from './../../../services/grid-layout-service/grid-layout.service';
 import { StoryType } from './../../../customtypes/story-type';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy, Pipe } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit, OnDestroy {
+  /**
+   * Subject used to unsubscribe from the subscription to the
+   * gridLayoutService.observeBreakpoints() Observable
+   * @private
+   * @type {Subject<void>}
+   * @memberof ToolbarComponent
+   */
+  private destroyed$: Subject<void> = new Subject();
+
   /**
    * The Event Emitter passes the emiited value to its parent
    * The Emitter emits a value when a story is selected from the toolbar
@@ -16,14 +28,31 @@ export class ToolbarComponent {
   @Output() storyTypeSelectEmitter: EventEmitter<StoryType> = new EventEmitter();
 
   /**
-   * Array of literal types used in the temnpate to display them in the toolbar
+   * Array of literal types used in the template to display them in the toolbar
    * @type {((StoryType)[])}
    * @memberof ToolbarComponent
    */
   storyTypes: StoryType[]
-    = ['topstories' , 'newstories' , 'beststories' , 'askstories' , 'showstories' , 'jobstories'];
+    = ['topstories', 'newstories', 'beststories', 'askstories', 'showstories', 'jobstories'];
 
-  constructor() {}
+  /**
+   * Boolean flag that hides/unhides the toolbar stories and menu for smaller screen
+   * @type {boolean}
+   * @memberof ToolbarComponent
+   */
+  isSmallerScreen: boolean = false;
+
+  constructor(private gridLayoutService: GridLayoutService) {}
+
+  ngOnInit(): void {
+    // Subscribing to the Observable<BreakpointState>, setting the isSmallerScreen property
+    this.gridLayoutService.observeBreakpoints()
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((state: BreakpointState) => {
+        this.isSmallerScreen = (state.breakpoints[Breakpoints.XSmall] || state.breakpoints[Breakpoints.Small] ||
+          state.breakpoints[Breakpoints.Medium])? true: false;
+      });
+  }
 
   /**
    * Event handler for the click event on the toolbar story types
@@ -31,5 +60,13 @@ export class ToolbarComponent {
    */
   onStoryClickHandler(storyType: StoryType) {
     this.storyTypeSelectEmitter.emit(storyType);
+  }
+
+  /**
+   * Clean up the subscription
+   */
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
   }
 }
