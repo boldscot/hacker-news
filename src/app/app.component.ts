a6c9b5644a132000429594ceb6d943c460ed1036
Stephen <bold-scot@hotmail.com>
2022-03-25 00:31:06 +0000

    Removed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div class="front-page">
+      <app-front-page></app-front-page>
+    </div>
+  `,
+  styles: [
+    `.front-page{
+      background: url(../assets/images/bg.jpg) no-repeat center center fixed;
+      min-height: 100%;
+      min-width: 100%;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+    }`,
+  ]
 })
-export class AppComponent {
-  title = 'hacker-news';
-}
+export class AppComponent {}
