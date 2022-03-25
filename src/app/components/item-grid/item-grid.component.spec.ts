3224c1cf7b066d2277e0d2b72ca719472e061ee3
Stephen <bold-scot@hotmail.com>
2022-03-25 00:25:44 +0000

    Added item grid tests

--- src/app/components/item-grid/item-grid.component.spec.ts
+++ src/app/components/item-grid/item-grid.component.spec.ts
@@ -1,3 +1,4 @@
+import { AppComponent } from './../../app.component';
 import { MockGridLayoutService } from './../../testutils/mock-grid-layout-service';
 import { GridLayoutService } from './../../services/grid-layout-service/grid-layout.service';
 import { ComponentsModule } from './../components.module';
@@ -6,6 +7,9 @@ import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testin
 import { HackerNewsService } from 'src/app/services/hacker-news-service/hacker-news.service';
 import { MockHackerNewsService } from 'src/app/testutils';
 import { ItemGridComponent } from './item-grid.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MatGridList, MatGridTile } from '@angular/material/grid-list';
 
 describe('ItemGridComponent', () => {
   let component: ItemGridComponent;
@@ -30,6 +34,7 @@ describe('ItemGridComponent', () => {
 
     fixture = TestBed.createComponent(ItemGridComponent);
     component = fixture.componentInstance;
+    component.stories$ = mockHackerNewsService.getStories();
     fixture.detectChanges();
   });
 
@@ -40,17 +45,16 @@ describe('ItemGridComponent', () => {
   it('should populate the stories$ Observable and set the gridFirstItemIndex to zero', () => {
     expect(component.gridFirstItemIndex).withContext('is initialised to 0').toBe(0);
     component.gridFirstItemIndex = 100;
-    component.storyType =  'newstories';
+    component.storyType = 'newstories';
     fixture.detectChanges();
     expect(component.gridFirstItemIndex).toBe(0);
-    component.stories$.subscribe((ids: number[] | null)  => {
-      expect(ids).toEqual([1,2,3,4,5,6,7,8,9])
+    component.stories$.subscribe((ids: number[] | null) => {
+      expect(ids).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
     });
   });
 
-  it('#updateFirstGridIndex() should increment the updateGridFirstItemIndex$ property', fakeAsync (() => {
+  it('#updateFirstGridIndex() should increment the gridFirstItemIndex property', fakeAsync(() => {
     component.gridFirstItemIndex = 1000;
-    component.gridLayout = mockGridLayoutService.getGridLayout();
     component.updateFirstGridIndex(true);
     fixture.detectChanges()
 
@@ -62,9 +66,8 @@ describe('ItemGridComponent', () => {
     expect(component.gridFirstItemIndex).toBe(1027);
   }));
 
-  it('#updateFirstGridIndex() should decrement the updateGridFirstItemIndex$ property', fakeAsync (() => {
+  it('#updateFirstGridIndex() should decrement the gridFirstItemIndex property', fakeAsync(() => {
     component.gridFirstItemIndex = 527;
-    component.gridLayout = mockGridLayoutService.getGridLayout();
     component.updateFirstGridIndex(false);
     fixture.detectChanges()
 
@@ -76,5 +79,61 @@ describe('ItemGridComponent', () => {
     expect(component.gridFirstItemIndex).toBe(500);
   }));
 
+  it('clicking the previous button should decrement the gridFirstItemIndex property', fakeAsync(() => {
+    component.gridFirstItemIndex = 227;
+    let de: DebugElement = fixture.debugElement.query(By.css('.prev'));
+    let spy: jasmine.Spy = spyOn(component, 'updateFirstGridIndex').and.callThrough();
 
+    component.updateGridFirstItemIndex$.subscribe((isIncrement: boolean) => {
+      expect(isIncrement).toBeFalsy();
+    });
+    de.triggerEventHandler('click', false);
+    // debounce Time
+    tick(300);
+    expect(component.gridFirstItemIndex).toBe(200);
+    expect(spy).toHaveBeenCalled();
+  }));
+
+  it('clicking the more button should increment the gridFirstItemIndex property', fakeAsync(() => {
+    component.gridFirstItemIndex = 40;
+    let de: DebugElement = fixture.debugElement.query(By.css('.more'));
+    let spy: jasmine.Spy = spyOn(component, 'updateFirstGridIndex').and.callThrough();
+
+    component.updateGridFirstItemIndex$.subscribe((isIncrement: boolean) => {
+      expect(isIncrement).toBeTruthy();
+    });
+    de.triggerEventHandler('click', true);
+    // debounce Time
+    tick(300);
+    expect(component.gridFirstItemIndex).toBe(67);
+    expect(spy).toHaveBeenCalled();
+  }));
+
+  it('#gridLayout settings should be used to lay out the grid', () => {
+    let de: DebugElement = fixture.debugElement.query(By.directive(MatGridList));
+    let el: HTMLElement = de.nativeElement;
+
+    expect(el.getAttribute('ng-reflect-cols'))
+      .withContext('angulars class name for the cols input')
+      .toBe('3');
+
+    expect(el.getAttribute('ng-reflect-row-height'))
+      .withContext('angulars class name for the rowHeight input')
+      .toBe('11rem');
+
+    expect(el.getAttribute('ng-reflect-gutter-size'))
+      .withContext('angulars class name for the gutterSize input')
+      .toBe('1rem');
+  });
+
+  it('the grid should have 9 grid tiles which contain an item each', () => {
+    let des: DebugElement[] = fixture.debugElement.queryAll(By.directive(MatGridTile));
+    expect(des).not.toBeNull();
+    expect(des.length).toBe(9);
+
+    des = fixture.debugElement.queryAll(By.css('.item'));
+    expect(des).not.toBeNull();
+    expect(des.length).toBe(9);
+
+  });
 });
