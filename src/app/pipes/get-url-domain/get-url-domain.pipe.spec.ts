023a3d3882eac2413a3403f31861a2dbf4691e8d
Stephen <bold-scot@hotmail.com>
2022-03-26 11:51:03 +0000

    Added getUrlDomainPipe tests

---
+++ src/app/pipes/get-url-domain/get-url-domain.pipe.spec.ts
@@ -0,0 +1,24 @@
+import { GetUrlDomainPipe } from './get-url-domain.pipe';
+
+describe('GetUrlDomainPipe', () => {
+  const pipe = new GetUrlDomainPipe();
+
+  it('should return google.com', () => {
+    expect(pipe.transform('https://www.google.com')).toEqual('google.com');
+  });
+
+  it('should return hacker-news.firebaseio.com', () => {
+    expect(pipe.transform('https://hacker-news.firebaseio.com/v0//item/30810410.json'))
+      .toEqual('hacker-news.firebaseio.com');
+  });
+
+  it('should return fake-domain.ie', () => {
+    expect(pipe.transform('http://fake-domain.ie/v0/'))
+      .toEqual('fake-domain.ie');
+  });
+
+  it('should return thewitcher.com', () => {
+    expect(pipe.transform('https://www.thewitcher.com/en/news'))
+      .toEqual('thewitcher.com');
+  });
+})
--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,4 +1,4 @@
-import { GetUrlDomainPipe } from './get-url-domain.pipe';
+import { GetUrlDomainPipe } from './get-url-domain/get-url-domain.pipe';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TimeAgoPipe } from './time-ago/time-ago.pipe';
